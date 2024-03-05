2666cca7d06294d1f9b80e4d96f53c526f8a0471
tmg0 <zekun.jin@foxmail.com>
2024-03-05 14:45:11 +0800

    feat: use rollup utils filter includes

--- src/unplugin.ts
+++ src/unplugin.ts
@@ -1,9 +1,31 @@
 import { createUnplugin } from 'unplugin'
+import type { FilterPattern } from '@rollup/pluginutils'
+import { createFilter } from '@rollup/pluginutils'
 
-const unplugin = createUnplugin(() => {
+export interface UnpluginTodosOptions {
+  include: FilterPattern
+  exclude: FilterPattern
+}
+
+export const defaultIncludes = [/\.[jt]sx?$/, /\.vue$/, /\.vue\?vue/, /\.svelte$/]
+export const defaultExcludes = [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/]
+
+const toArray = <T>(x: T | T[] | undefined | null): T[] => x == null ? [] : Array.isArray(x) ? x : [x]
+
+const unplugin = createUnplugin<Partial<UnpluginTodosOptions>>((options = {}) => {
+  const filter = createFilter(
+    toArray(options.include as string[] || []).length
+      ? options.include
+      : defaultIncludes,
+    options.exclude || defaultExcludes,
+  )
+
   return {
     name: 'todos',
     enforce: 'post',
+    transformInclude(id) {
+      return filter(id)
+    },
   }
 })
 
