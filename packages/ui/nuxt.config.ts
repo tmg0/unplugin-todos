f0555c27ed979ae07e2172d459f516303bdacc32
tmg0 <zekun.jin@foxmail.com>
2024-03-07 13:04:57 +0800

    chore: update output dir

--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -1,3 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/_todos',
+  },
+
+  nitro: {
+    output: {
+      dir: '../../dist',
+    },
+  },
 })
