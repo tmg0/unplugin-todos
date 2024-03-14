2677a275b4ce0250e5dad78e99b057692517f555
tmg0 <zekun.jin@foxmail.com>
2024-03-14 11:19:55 +0800

    chore: update table tr base ui

--- packages/ui/app.config.ts
+++ packages/ui/app.config.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
       thead: 'z-10',
       tbody: 'divide-y-0',
       tr: {
-        base: 'even:bg-white/50',
+        base: 'even:bg-white/50 dark:even:bg-white/5',
       },
       th: {
         base: 'first:rounded-l-lg last:rounded-r-lg bg-white dark:bg-white/10 overflow-hidden',
